3c45d8616ced8dc9dd0623fe4bbecb601c3151e1
Sebastian Sanmiguel <js.garcias1@uniandes.edu.co>
2026-02-07 08:49:01 -0500

    commitAsocaciones

--- src/main/java/co/edu/uniandes/dse/TallerPersistencia/entities/DirectorEntity.java
+++ src/main/java/co/edu/uniandes/dse/TallerPersistencia/entities/DirectorEntity.java
@@ -2,15 +2,17 @@ package co.edu.uniandes.dse.TallerPersistencia.entities;
 
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
 
 @Data
 @Entity
 public class DirectorEntity extends BaseEntity {
     
-    private Long id_director;
+   
     private String nombre;
     private String biografia;
-
+    @ManyToOne
+    PeliculaEntity pelicula; 
 
 }
--- src/main/java/co/edu/uniandes/dse/TallerPersistencia/entities/PeliculaEntity.java
+++ src/main/java/co/edu/uniandes/dse/TallerPersistencia/entities/PeliculaEntity.java
@@ -1,20 +1,23 @@
 package co.edu.uniandes.dse.TallerPersistencia.entities;
-
+import java.util.List;
+import java.util.ArrayList;
 import jakarta.persistence.Entity;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.FetchType;
+
+import jakarta.persistence.OneToMany;
 import lombok.Data;
-/* 
+
 @Data
 @Entity
 public class PeliculaEntity extends BaseEntity {
     
 
-
+    
     private String titulo;
     private int anioLanzamiento;
 
-    @OneToMany
-    @JoinColumn(mappedBy = "id_director")
+    @OneToMany(mappedBy =  "nombre" , fetch = FetchType.LAZY)
+    List<DirectorEntity> peliculasDirector = new ArrayList<>();
 
 
-}*/
+}
